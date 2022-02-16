176e6ee42db6b9f2fe28853e9173cba2acb551ac
Subhodeep <subhodeeppal64@gmail.com>
2022-02-16 17:21:03 +0530

    Permission Accessibility

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.filemanager;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -27,7 +31,83 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        String[] PERMISSIONS = new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        // Works only for build version is Android 10 or less than that
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            requestPermissions(PERMISSIONS, REQUEST_CODE);
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean isPermissionGranted;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            isPermissionGranted = Environment.isExternalStorageManager();
+            if (!isPermissionGranted) {
+                new AlertDialog.Builder(this)
+                        .setTitle("All Files Permission")
+                        .setMessage("Due to Android 11 restrictions this app requires all files permission")
+                        .setPositiveButton("Allow", (dialogInterface, i) -> takePermission())
+                        .setNegativeButton("Deny", (dialogInterface, i) -> finish())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            } else {
+                Toast.makeText(this, "Permission Already Granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
+    private void takePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            try {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.addCategory("android.intent.category.DEFAULT");
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivityForResult(intent, 101);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                startActivityForResult(intent, 101);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted. Continue the action or workflow
+                // in your app.
+                Toast.makeText(MainActivity.this, "Read external storage permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // Explain to the user that the feature is unavailable because
+                // the features requires a permission that the user has denied.
+                // At the same time, respect the user's decision. Don't link to
+                // system settings in an effort to convince the user to change
+                // their decision.
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+                builder.setCancelable(false);
+                // set ok button on alert dialog
+                builder.setPositiveButton(R.string.dialog_ok, (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE));
+
+                // set cancel on alert dialog
+                builder.setNegativeButton(R.string.dialog_cancel, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                    finish();
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        }
     }
 }
