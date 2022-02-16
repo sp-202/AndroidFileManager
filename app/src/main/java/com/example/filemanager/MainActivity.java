96f67a353e3ec35c84e83b7018570dddf6e5e7ac
Subhodeep <subhodeeppal64@gmail.com>
2022-02-16 13:14:34 +0530

    Permission Accessibility

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,50 +27,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        String[] PERMISSIONS = new String[]{
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-        };
-        // Works only for build version is Android 10 or less than that
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermissions(PERMISSIONS, REQUEST_CODE);
-            Toast.makeText(MainActivity.this, "Triggered", Toast.LENGTH_SHORT).show();
-        }
-
-        // For Android 11 And higher version
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE) {// If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0 &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission is granted. Continue the action or workflow
-                // in your app.
-                Toast.makeText(MainActivity.this, "Read external storage permission granted", Toast.LENGTH_SHORT).show();
-            } else {
-                // Explain to the user that the feature is unavailable because
-                // the features requires a permission that the user has denied.
-                // At the same time, respect the user's decision. Don't link to
-                // system settings in an effort to convince the user to change
-                // their decision.
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
-                builder.setCancelable(false);
-                // set ok button on alert dialog
-                builder.setPositiveButton(R.string.dialog_ok, (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE));
-
-                // set cancel on alert dialog
-                builder.setNegativeButton(R.string.dialog_cancel, (dialogInterface, i) -> {
-                    dialogInterface.dismiss();
-                    finish();
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
-        }
     }
 }
