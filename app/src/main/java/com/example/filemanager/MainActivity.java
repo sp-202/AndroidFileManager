202609c10a195e99291208d5e1da9fc9cea88bff
Subhodeep <subhodeeppal64@gmail.com>
2022-02-16 17:02:19 +0530

    Permission Accessibility

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -30,13 +30,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        String[] PERMISSIONS = new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        // Works only for build version is Android 10 or less than that
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            requestPermissions(PERMISSIONS, REQUEST_CODE);
+            Toast.makeText(MainActivity.this, "Triggered", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         if (!Utils.isPermissionGranted(MainActivity.this) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             new AlertDialog.Builder(this)
                     .setTitle("All Files Permission")
@@ -64,22 +70,39 @@ public class MainActivity extends AppCompatActivity {
                 intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 startActivityForResult(intent, 101);
             }
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
-                    101);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0) {
-            if (requestCode == 101) {
-                boolean readExt = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                if (!readExt) {
-                    takePermission();
-                }
+        if (requestCode == REQUEST_CODE) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted. Continue the action or workflow
+                // in your app.
+                Toast.makeText(MainActivity.this, "Read external storage permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // Explain to the user that the feature is unavailable because
+                // the features requires a permission that the user has denied.
+                // At the same time, respect the user's decision. Don't link to
+                // system settings in an effort to convince the user to change
+                // their decision.
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
+                builder.setCancelable(false);
+                // set ok button on alert dialog
+                builder.setPositiveButton(R.string.dialog_ok, (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE));
+
+                // set cancel on alert dialog
+                builder.setNegativeButton(R.string.dialog_cancel, (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                    finish();
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         }
     }
