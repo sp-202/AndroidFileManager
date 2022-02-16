1ebf73bbd561cde7107edeefacad2be6e7ea0ac7
Subhodeep <subhodeeppal64@gmail.com>
2022-02-16 10:07:27 +0530

    16-02-2022

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.filemanager;
 
 import android.Manifest;
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -14,27 +18,69 @@ import com.example.filemanager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+
+    BluetoothAdapter bluetoothAdapter;
+
     private static final int STORAGE_PERMISSION_CODE = 1;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (ContextCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // You can use the API that requires the permission.
+            // performAction(...);
+//            binding.textView.setText("Welcome to app1");
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
     }
 
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        return result == PackageManager.PERMISSION_GRANTED;
-    }
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case STORAGE_PERMISSION_CODE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    Toast.makeText(MainActivity.this, "Welcome to the app", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage(R.string.dialog_message).setTitle(R.string.dialog_title);
 
-    private void Request_Permission() {
+                    // set ok button on alert dialog
+                    builder.setPositiveButton(R.string.dialog_ok, (dialogInterface, i) -> ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE));
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this, "Storage Permission is required", Toast.LENGTH_SHORT).show();
-        } else
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                    // set cancel on alert dialog
+                    builder.setNegativeButton(R.string.dialog_cancel, (dialogInterface, i) -> {
+                        dialogInterface.dismiss();
+                        finish();
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+        }
+        // Other 'case' lines to check for other
+        // permissions this app might request.
     }
 }
